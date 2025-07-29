12b8b6294ee052e0bcaaf87e76bf28cfa5310ab0
Md. Saminul Amin <saminul.amin@gmail.com>
2025-07-29 20:48:08 +0600

    setup done

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import { router } from "./app/routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/api", router);
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Welcome to my backend verse!",
   });
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
